74a46a14394296d4c9fe519c7d7234fc1a84b795
Angélica Olvera <aolvera163@gmail.com>
2018-06-03 16:18:27 +0200

    heroku 93

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Serve only the static files form the dist directory
 
-app.use(express.static('/dist'));
+app.use(express.static('/dist/demo-deploy'));
 
 app.get('/*', function(req,res) {
   res.sendFile(path.join('/dist/demo-deploy/index.html'));
