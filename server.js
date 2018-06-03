118f8d97517eb30ebb026c83c3acc6cf50d476f4
Angélica Olvera <aolvera163@gmail.com>
2018-06-03 16:30:18 +0200

    heroku 10

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 //Install express server
 const express = require('express');
+const http = require('http');
 const path = require('path');
 
 const app = express();
 
 // Serve only the static files form the dist directory
 
-app.use(express.static('/dist/demo-deploy'));
+app.use(express.static(path.join(__dirname, 'dist/demo-deploy')));
 
-app.get('/*', function(req,res) {
-  res.sendFile(path.join('/dist/demo-deploy/index.html'));
+app.get('*', function(req,res) {
+  res.sendFile(path.join(__dirname, '/dist/demo-deploy/index.html'));
 });
 
+const port = process.env.PORT || 8080;
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, () => console.log('running'));
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+//app.listen(process.env.PORT || 8080);
